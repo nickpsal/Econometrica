1e29b4e312f86affff142d9bc7a460423c26adbe
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2020-03-07 15:45:37 +0200

    29/02/2020

--- src/Ecometrica/Views/InsertDataGUI.java
+++ src/Ecometrica/Views/InsertDataGUI.java
@@ -685,7 +685,9 @@ public class InsertDataGUI extends javax.swing.JFrame {
             //Παιρνάμε τιμές στον Πίνακα
             Object[] row2 = {
                 OILdata.get(i).getDataYear(),
-                OILdata.get(i).getValue()
+                //Φορμαρισμα τιμης να εμφανίζει 2 δεδαδικά
+                df.format(Float.parseFloat(OILdata.get(i).getValue()))
+                //OILdata.get(i).getValue()
             };
             tm2.addRow(row2);
         }
